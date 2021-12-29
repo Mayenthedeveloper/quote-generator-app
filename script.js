80c41bf0f6a5f0ee9899427c656617840505e1bd
Mayen Akpan <neroxma@yahoo.com>
2021-12-29 15:34:39 -0800

    Dom Manipulation

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const authorText = document.getElementById("author");
+
 let apiQuotes = [];
 
 //show new quotes
 function newQuotes() {
   //pick a random quotes from api
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  //   check if author field is blank
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // chheck quote lenght to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
@@ -18,5 +38,15 @@ async function getQuotes() {
     //catch error here
   }
 }
+// tweet a quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// event listener
+newQuoteBtn.addEventListener("click", newQuotes);
+twitterBtn.addEventListener("click", tweetQuote);
+
 //on load
 getQuotes();
